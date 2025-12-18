6efad124cd86f589ef9d7c7a91464377156ccb86
Andrey Mendes <andreymendes903@gmail.com>
2025-12-18 16:51:27 -0300

    Adiciona ficha pessoal em Python

---
+++ FichaPessoall.py
@@ -0,0 +1,14 @@
+# Projeto: Ficha Pessoal em Python
+
+nome = "Maicon"
+idade = 21
+cidade = "Rio de Janeiro"
+cpf = "234.567.876-23"
+data_de_nascimento = "19/02/2002"
+
+print("===== FICHA PESSOAL =====")
+print("Nome:", nome)
+print("Idade:", idade)
+print("Cidade:", cidade)
+print("CPF:", cpf)
+print("Data de nascimento:", data_de_nascimento)
